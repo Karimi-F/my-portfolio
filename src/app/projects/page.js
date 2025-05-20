e9e6391350c33bfa0cc18e24067270b9c5fa8e98
Karimi-F <fridah37karimi@gmail.com>
2025-05-20 22:52:29 +0300

    Adds the projects page

---
+++ src/app/projects/page.js
@@ -0,0 +1,31 @@
+"use client"
+
+export default function Projects(){
+  return (
+    <div>
+            <h1 className="text-center">PROJECTS</h1>
+            {/* <div className="flex justify-around">
+              
+              <h4>Mobile App</h4> 
+              <h4>Website</h4>  
+              <h4>Desktop</h4>  
+              <h4>Other Projects</h4>  
+            </div> */}
+            <div>
+                <h5>
+                  <a href="https://hanzi-flashcards.vercel.app/">Hanzi Flashcards</a></h5>
+                <div>
+                  <p></p>
+                </div>
+              </div>
+            <div>
+                <h5>
+                  <a href="https://food-court-group-2-6mbz.vercel.app/">BiteScape Food Court</a></h5>
+                <div>
+                  <p></p>
+                </div>
+              </div>
+        </div>
+  )
+    
+}
